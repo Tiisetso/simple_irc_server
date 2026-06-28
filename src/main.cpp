d6980d72dd05b23be20a44d7ab53d9ad56299d7c
Tiisetso Daniel Murray <timurray@student.hive.fi>
2026-06-28 15:41:47 +0300

    Dev (#12)
    * docs: TODO project requirements
    
    * feat: prepare address info and create server socket (#11)
    
    * docs: add server socket notes
    
    * feat: prepare address info and create server socket
    
    * chore: update gitignore
    
    * refactor: throw exception for server setup error
    
    * refactor: throw exception for server setup error
    
    ---------
    
    Co-authored-by: Yuan <justyuan.z@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
-
+#include "Server.hpp"
+#include <exception>
 #include <iostream>
 
-int main(void)
+int main(int ac, char **av)
 {
-	//takes in port number.
-	std::cout << "Hello IRC!" << std::endl;
-	return (EXIT_SUCCESS);
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./ircserv <port>" << std::endl;
+		return 1;
+	}
+
+	std::string port = av[1];
+
+	try
+	{
+		Server server(port);
+		server.createSocket();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
+
+	return 0;
 }
