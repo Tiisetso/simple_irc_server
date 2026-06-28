e1f5ab52bc546ae0dafa0a52acb002b8ecf54659
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2026-06-28 15:02:41 +0300

    wip: port valid check.

--- src/parse_port.cpp
+++ src/parse_port.cpp
@@ -4,7 +4,20 @@
 
 bool parsePort(const std::string& portString)
 {
+	unsigned int port = 0;
+	char c;
+
 	if (portString.empty())
 		return (false);
-	return (true);
+
+	for (std::size_t i = 0; i < portString.length(); ++i)
+	{
+		c = portString[i];
+		if (c < '0' || c > '9')
+			return (false);
+		port = port * 10 + (c - '0');
+		if (port > 65535)
+			return (false);
+	}
+	return (port > 0);
 }
