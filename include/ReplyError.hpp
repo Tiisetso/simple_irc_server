84e4d803a56cfa116e682b5fb7c28ab8fd4e9659
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2026-07-26 21:25:57 +0300

    feat: new error messaes for PART

--- include/ReplyError.hpp
+++ include/ReplyError.hpp
@@ -5,20 +5,24 @@
 
 enum errReplyCode
 {
+	ERR_NOSUCHCHANNEL = 403,
 	ERR_NOORIGIN = 409,
 	ERR_NONICKNAMEGIVEN = 431,
 	ERR_ERRONEUSNICKNAME = 432,
 	ERR_NICKNAMEINUSE = 433,
+	ERR_NOTONCHANNEL = 442,
 	ERR_NEEDMOREPARAMS = 461,
 	ERR_ALREADYREGISTERED = 462,
 	ERR_PASSWDMISMATCH = 464,
 };
 
 const std::unordered_map<int, std::string> errReplyMsg = {
+	{ERR_NOSUCHCHANNEL, "No such channel"},
 	{ERR_NOORIGIN, "No origin specified"},
 	{ERR_NONICKNAMEGIVEN, "No nickname given"},
 	{ERR_ERRONEUSNICKNAME, "Erroneous nickname"},
 	{ERR_NICKNAMEINUSE, "Nickname is already in use"},
+	{ERR_NOTONCHANNEL, "You're not on that channel"},
 	{ERR_NEEDMOREPARAMS, "Not enough parameters"},
 	{ERR_ALREADYREGISTERED, "You may not reregister"},
 	{ERR_PASSWDMISMATCH, "Password incorrect"},
