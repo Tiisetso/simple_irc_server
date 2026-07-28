cacf07868c0222f398db7b79698b01d90337e82c
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2026-07-28 15:23:58 +0300

    wip: quit skeleton.

---
+++ src/Quit.cpp
@@ -0,0 +1,8 @@
+#include "Server.hpp"
+
+void Server::handleQuit(const command &cmd, User &client)
+{
+	//TODO: collect users in same channel. No dupes.
+	//TODO: broadcast quit message.
+	//TODO: mark client for deletion.
+}
