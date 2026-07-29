af80f2794fc7a83c3670c0c127dbaaa1242291bf
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2026-07-30 01:06:56 +0300

    feat: quit handler. style: formatting.

--- src/Quit.cpp
+++ src/Quit.cpp
@@ -5,5 +5,5 @@ void Server::handleQuit(const command &cmd, User &client)
 	std::string disconnectReason = "Client quit";
 	if (!cmd.vals.empty() && !cmd.vals[0].empty())
 		disconnectReason = cmd.vals[0];
-	client.setShouldDisconnect("Quit: " + disconnectReason);
+	client.setShouldDisconnect(disconnectReason);
 }
