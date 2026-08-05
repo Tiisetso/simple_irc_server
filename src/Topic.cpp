646ea6499e4d71bcb8b110b8e4142cf186fa350c
slowTake <pnurmi.zt@gmail.com>
2026-08-05 21:22:55 +0300

    topic handler wip

--- src/Topic.cpp
+++ src/Topic.cpp
@@ -1,7 +1,36 @@
+#include "ReplyError.hpp"
 #include "Server.hpp"
 
 void Server::handleTopic(const command &cmd, User &client)
 {
-	(void)cmd;
-	(void)client;
+	if (cmd.vals.empty())
+	{
+		queueMessage(client, msgReply(client, ERR_NEEDMOREPARAMS, cmd.key));
+		return;
+	}
+
+	const std::string &channelName = cmd.vals[0];
+	Channel *channel = getChannel(channelName);
+	if (!channel)
+	{
+		queueMessage(client, msgReply(client, ERR_NOSUCHCHANNEL, channelName));
+		return;
+	}
+
+	if (!channel->isUserInChannel(client))
+	{
+		queueMessage(client,
+					 msgReply(client, ERR_NOTONCHANNEL, channel->getName()));
+		return;
+	}
+
+	if (cmd.vals.size() >= 2)
+		return;
+
+	if (channel->getTopic().empty())
+		queueMessage(client, msgNumeric(client, 331, channel->getName(),
+										"No topic is set"));
+	else
+		queueMessage(client, msgNumeric(client, 332, channel->getName(),
+										channel->getTopic()));
 }
